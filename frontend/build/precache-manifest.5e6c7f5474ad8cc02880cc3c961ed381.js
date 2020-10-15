a6a8587b7af6bdcf7f3769500f4bb0a18e8c8e88
Lachlan <lachlan@lachlanb.com>
2020-10-15 16:25:01 +1100

    commiting build files

---
+++ frontend/build/precache-manifest.5e6c7f5474ad8cc02880cc3c961ed381.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a132eea97b503788db90f44f7231b97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20e637bbff77f5f5939c",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "052802fb160fda2bb4f2",
+    "url": "/static/css/main.2a1311b1.chunk.css"
+  },
+  {
+    "revision": "20e637bbff77f5f5939c",
+    "url": "/static/js/2.53090b09.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.53090b09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "052802fb160fda2bb4f2",
+    "url": "/static/js/main.89b5fc3c.chunk.js"
+  },
+  {
+    "revision": "e744f14f3a88f64dcdb4",
+    "url": "/static/js/runtime-main.ea513989.js"
+  }
+]);
